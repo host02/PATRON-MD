11d6c79080c9e90d5ea8ef79a48f43196b9a0bf6
host02 <ponguitatie@gmail.com>
2025-02-15 23:08:57 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "JOS"
 
-global.banknumber = "8133729715"
+global.banknumber = "527483828273938"
 
-global.bankowner = "FADARE"
+global.bankowner = "Patron"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
